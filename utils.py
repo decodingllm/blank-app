dab168f92d70475eebb9b532d81b2c288f56584c
decodingllm <decodingllm@gmail.com>
2025-02-15 11:01:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+import streamlit as st
+import http.client
+import urllib.parse
+import json
+import os
+
 def extract_transcript_text(apiResponse):
     transcript_text = []
     if isinstance(apiResponse, dict):
@@ -10,3 +16,25 @@ def extract_transcript_text(apiResponse):
         for item in apiResponse:
             transcript_text.extend(extract_transcript_text(item))
     return transcript_text
+
+
+def get_transcript_with_params(video_id, api_key, rapidapi_host, platform):
+    conn = http.client.HTTPSConnection("youtube-video-summarizer-gpt-ai.p.rapidapi.com") # API endpoint
+
+    headers = {
+        'x-rapidapi-key': api_key,
+        'x-rapidapi-host': rapidapi_host
+    }
+
+    # Construct the URL with dynamic parameters
+    params = {
+        'video_id': video_id,
+        'platform': platform
+    }
+    #params.update(platform)  # Adding platform selection
+    url = "/api/v1/get-transcript-v2?" + urllib.parse.urlencode(params)  # encoding parameters
+
+    conn.request("GET", url, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    return data.decode("utf-8")
